ea0065a272694d7f7d6c4c3182f4758ec8be04ee
esegebart <36378900+esegebart@users.noreply.github.com>
2019-10-13 16:55:59 -0500

    Add files to track

---
+++ app/src/main/java/com/elyse/cleveland_museum_api/MainActivity.java
@@ -0,0 +1,14 @@
+package com.elyse.cleveland_museum_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
